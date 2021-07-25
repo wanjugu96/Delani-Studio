a26f9621d92f79fb0989a4f3fe4581a536e13ab7
wanjugu mung'au <wanjugu.mung'au@strathmore.edu>
2021-07-25 17:08:15 +0300

    Adding toggle functions

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    $(".clickable").click(function() {
+
+        $(".image").toggle();
+        $(".words").toggle();
+
+    })
+    $(".clickable1").click(function() {
+
+        $(".image1").toggle();
+        $(".words1").toggle();
+
+    })
+    $(".clickable2").click(function() {
+
+        $(".image2").toggle();
+        $(".words2").toggle();
+
+    })
+
+
+})
